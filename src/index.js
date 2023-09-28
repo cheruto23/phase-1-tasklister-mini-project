39440b6fefb5f062f7259464997b1d6bdff3b2a3
Joy <joyglory213@gmail.com>
2023-09-28 14:36:27 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    toDohandler(e.target.elements['new-task-description'].value);
+    form.reset();
+  });
 });
+
+
+function toDohandler(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteHandler);
+  btn.textContent = 'X';
+  p.textContent = `${todo}`;
+  p.appendChild(btn);
+
+  document.querySelector('#list').appendChild(p);
+}
+
+function deleteHandler(e){
+  e.target.parentNode.remove();
+}
